441efb53f7298258561492a7c392dcf070f70e80
Tarik-Colakhodzic <tarik.colakhodzic@edu.fit.ba>
2022-01-14 00:19:02 +0100

    Regex validation for credit card number and cvc added

--- lib/pages/PropertyDetails.dart
+++ lib/pages/PropertyDetails.dart
@@ -434,6 +434,8 @@ class _PropertyDetailsState extends State<PropertyDetails> {
                           validator: (value) {
                             if (value == null || value.isEmpty)
                               return 'Broj kartice je obavezan!';
+                            if (!isCardNumberValid(value))
+                              return 'Broj kartice nije u ispravnom formatu!';
                             else
                               return null;
                           },
@@ -448,6 +450,8 @@ class _PropertyDetailsState extends State<PropertyDetails> {
                           validator: (value) {
                             if (value == null || value.isEmpty)
                               return 'CVC je obavezan!';
+                            if(!isCVCValid(value))
+                              return 'CVC nije u ispravnom formatu!';
                             else
                               return null;
                           },
@@ -515,7 +519,8 @@ class _PropertyDetailsState extends State<PropertyDetails> {
                                   'Transakcija uspješno prošla. Čestitamo, upravo ste postali vlasnik ove nekretnine!'),
                               actions: <Widget>[
                                 TextButton(
-                                  onPressed: () => Navigator.of(context).pushNamed('/properties'),
+                                  onPressed: () => Navigator.of(context)
+                                      .pushNamed('/properties'),
                                   child: const Text('Ok'),
                                 ),
                               ],
@@ -543,5 +548,16 @@ class _PropertyDetailsState extends State<PropertyDetails> {
             },
           );
         });
+  }
+
+  bool isCardNumberValid(String number) {
+    RegExp regex = new RegExp(
+        r"^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$");
+    return regex.hasMatch(number);
+  }
+
+  bool isCVCValid(String cvc) {
+    RegExp regex = new RegExp(r"^\d{3}$");
+    return regex.hasMatch(cvc);
   }
 }
